7e2fbba6c706f8e555b3de1080505f3114c1448c
f0lie <9092074+f0lie@users.noreply.github.com>
2020-12-07 15:31:48 -0500

    added day binning

--- analysis_nouns.py
+++ analysis_nouns.py
@@ -10,8 +10,16 @@ def most_common_nouns(nouns, n=100):
     for noun, count in counter.most_common(n):
         print(noun, '\t', count)
 
-if __name__ == "__main__":
-    nouns = find_nouns.read_nouns()
+def time_bin(titles, bin_length=86400):
+    # Bins titles by created_utc. Does this in chunks of bin_length which is utc in day_length by default
+    start_time = min(int(title[2]) for title in titles)
+    bin = collections.defaultdict(list)
+    for row_id, _, created_utc in titles:
+        day = (int(created_utc)-start_time)//bin_length
+        bin[day].append(row_id)
+    return bin
 
-    most_common_nouns(nouns)
-    
+if __name__ == "__main__":
+    titles = find_nouns.read_titles()
+
+    print(time_bin(titles))
