78ed4b5b3fce4eb262a90b45c5c5ff33f01871d2
f0lie <9092074+f0lie@users.noreply.github.com>
2020-12-07 16:01:47 -0500

    added printing bins by day

--- analysis_nouns.py
+++ analysis_nouns.py
@@ -1,7 +1,9 @@
 import collections
 import find_nouns
+import datetime
 
-peusdo_stem_words = set(["reddit", "redditor", "people", "life", "story", "time", "person", "thing", "way"])
+#peusdo_stem_words = set(["reddit", "redditor", "people", "life", "story", "time", "person", "thing", "way"])
+
 
 def time_bin(titles, start_time, bin_length=86400):
     # Bins titles by created_utc. Does this in chunks of bin_length which is utc in day_length by default
@@ -14,8 +16,8 @@ def time_bin(titles, start_time, bin_length=86400):
 def counter_bin(titles_bin, nouns):
     noun_lookup = collections.defaultdict(list)
     for row_id, noun in nouns:
-        if noun.lower() not in peusdo_stem_words:
-            noun_lookup[row_id].append(noun.lower())
+        #if noun.lower() not in peusdo_stem_words:
+        noun_lookup[row_id].append(noun.lower())
 
     bin_count = collections.defaultdict(collections.Counter)
     for bin, titles in titles_bin.items():
@@ -23,6 +25,11 @@ def counter_bin(titles_bin, nouns):
             bin_count[bin].update(noun_lookup[str(title)])
     return bin_count
     
+def print_day_common_nouns(counter_bin, start_time, n=5):
+    for day, counter in counter_bin.items():
+        print(datetime.date.fromtimestamp(start_time + day*86400))
+        for noun, count in counter.most_common(n):
+            print('\t', noun, ":", count)
 
 if __name__ == "__main__":
     titles = find_nouns.read_titles()
@@ -34,5 +41,4 @@ if __name__ == "__main__":
 
     counter_bin = counter_bin(titles_bin, nouns)
 
-    for day, counter in counter_bin.items():
-        print(day, counter.most_common(10))
+    print_day_common_nouns(counter_bin, start_time)
