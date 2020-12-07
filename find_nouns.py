e56ac9212e3ec7840c87b98fed9f2771a1b447cc
f0lie <9092074+f0lie@users.noreply.github.com>
2020-12-07 15:02:15 -0500

    processed 100000 rows

--- find_nouns.py
+++ find_nouns.py
@@ -15,7 +15,7 @@ def read_titles(file_name='data/askreddit_titles_entire_month.csv', n=100000):
     return titles
 
 def write_nouns(found_nouns, file_name='data/found_nouns.csv'):
-    with open(file_name, 'w', newline='') as csv_file:
+    with open(file_name, 'w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['row_id', 'noun'])
         writer.writerows(found_nouns)
@@ -23,18 +23,24 @@ def write_nouns(found_nouns, file_name='data/found_nouns.csv'):
 if __name__ == "__main__":
     spacy.prefer_gpu()
     # Don't need NER pipeline
-    nlp = spacy.load("en_core_web_sm", disable=['ner'])
+    nlp = spacy.load("en_core_web_md", disable=['ner'])
 
+    print('Reading titles')
     titles = read_titles() 
+    print("Titles loaded")
 
     # Consist of tuples of (row_id, noun lemma) so it's possible to find sentence the noun is from
     found_nouns = []
 
+    print("Finding nouns")
     for row_id, title, _ in titles:
         doc = nlp(title)
         for token in doc:
             if token.pos_ == 'PROPN' or token.pos_ == 'NOUN':
                 found_nouns.append((row_id, token.lemma_))
+    print("Nouns found")
 
+    print("Writing nouns")
     write_nouns(found_nouns)
+    print("Nouns writen")
         
