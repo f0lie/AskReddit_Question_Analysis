8eac241e8a687ac0f7fe3be2dca7423061e3ef05
f0lie <9092074+f0lie@users.noreply.github.com>
2020-12-08 12:55:06 -0500

    wow OOP actually works.

--- analysis_nouns.py
+++ analysis_nouns.py
@@ -19,6 +19,8 @@ class TitleCount:
         self.count_nouns(remove_words)
     
     def update_titles(self, length):
+        # Updates groups_titles with new groups of a timespan of length
+        # Length should a timeperiod in seconds such as the length of a day in seconds, 86400
         self.group_titles = collections.defaultdict(list)
         self.length = length
         for row_id, _, created_utc in self.titles:
@@ -26,6 +28,8 @@ class TitleCount:
             self.group_titles[offset].append(row_id)
     
     def count_nouns(self, remove_words=None):
+        # Updates group_count_nouns with new counts of nouns within that group
+        # Remove_words is a set of words to remove from the count. Meant for very common words
         if not remove_words:
             remove_words = set()
         
@@ -41,70 +45,28 @@ class TitleCount:
             for title in titles:
                 self.group_count_nouns[group].update(noun_lookup[title])
  
-
-def group_titles(titles: List[Title], start_time: int, length: int=WEEK_LENGTH) -> Dict[int, List[str]]:
-    # Group titles by created_utc. Does this in chunks of length, defaults to week length. start_time should be the smallest timestamp
-    # Returns a dict with keys as the group and the value as a list of row_ids in that group
-    group_titles = collections.defaultdict(list)
-    for row_id, _, created_utc in titles:
-        # Subtract start_time from created_utc and divded by length to create offsets that are the groups
-        offset = (int(created_utc)-start_time)//length
-        group_titles[offset].append(row_id)
-    return group_titles
-
-def group_count_nouns(group_titles: Dict[int, List[str]], nouns: List[Noun], common_words: Set[str]=None) -> Dict[int, CountNouns]:
-    # Takes in grouped titles and counts all of the nouns with Counter. If a noun is in common_words, then it's removed from count.
-    # Returns a dict with keys as the group and the values as Counters of that group
-    if not common_words:
-        common_words = set()
-
-    # Create dict to quickly lookup the nouns that a question has
-    noun_lookup = collections.defaultdict(list)
-    for row_id, noun in nouns:
-        if noun.lower() not in common_words:
-            noun_lookup[row_id].append(noun.lower())
-
-    # Create a dict to quickly add counts to a grouping
-    group_count: Dict[int, CountNouns] = collections.defaultdict(collections.Counter)
-    for group, titles in group_titles.items():
-        for title in titles:
-            group_count[group].update(noun_lookup[title])
-    return group_count
-    
-def print_count_nouns(group_counter: Dict[int, CountNouns], start_time: int, length: int=WEEK_LENGTH, n: int=10) -> None :
-    # Prints most common nouns from bins. Length and start_time need to match the ones that created the group countings.
-    for group in sorted(group_counter):
-        # Convert the groups into readable timestamps
-        print(datetime.date.fromtimestamp(start_time + group*length))
-        for noun, count in group_counter[group].most_common(n):
-            print('\t', noun, ":", count)
+    def print_count(self, n=10):
+        # Prints out the top N most common words in a group
+        for group in sorted(self.group_count_nouns):
+            # Convert the groups into readable timestamps
+            print(datetime.date.fromtimestamp(self.start_time + group*self.length))
+            for noun, count in self.group_count_nouns[group].most_common(n):
+                print('\t', noun, ":", count)
 
 if __name__ == "__main__":
-    titles = find_nouns.read_titles("data/askreddit_titles_entire_month.csv", n=10)
+    titles = find_nouns.read_titles("data/askreddit_titles_entire_month.csv", n=None)
     nouns = find_nouns.read_nouns("data/found_nouns_all.csv")
     print("found titles:", len(titles), ", found nouns:", len(nouns))
 
-    """
-    # This is used to create offsets for the groupings. Allows for very fast grouping.
-    start_time = min(int(title[2]) for title in titles)
-    weekly_titles = group_titles(titles, start_time)
-
-    # These words show up very often in the result and don't say much so I removed them here.
+    title_count = TitleCount(titles, nouns, WEEK_LENGTH)
     common_words = set(["reddit", "redditor", "people", "life", "story", "time", "person", "thing", "way"])
-    no_common_count = group_count_nouns(weekly_titles, nouns, common_words)
-    all_count = group_count_nouns(weekly_titles, nouns)
 
-    print("Results with common words.")
-    print_count_nouns(all_count, start_time)
-    print("Results without common words.")
-    print_count_nouns(no_common_count, start_time)
-
-    print("Results with daily bins")
-    daily_titles = group_titles(titles, start_time, length=DAY_LENGTH)
-    daily_count = group_count_nouns(daily_titles, nouns)
-
-    print_count_nouns(daily_count, start_time, length=DAY_LENGTH)
-    """
-    title_count = TitleCount(titles, nouns, DAY_LENGTH)
-    print(title_count.group_titles)
-    print(title_count.group_count_nouns)
+    print("Results with common words")
+    title_count.print_count()
+    print("Results without common words")
+    title_count.count_nouns(common_words)
+    title_count.print_count()
+    print("Daily results")
+    title_count.update_titles(DAY_LENGTH)
+    title_count.print_count()
+ 
